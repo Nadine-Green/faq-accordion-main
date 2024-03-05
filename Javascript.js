dbc1d6872a1255101121a5449873cf73138d2704
Nadine-Green <103758182+Nadine-Green@users.noreply.github.com>
2024-03-05 12:59:34 +0200

    Deleted Unecessary comments

--- Javascript.js
+++ Javascript.js
@@ -1,34 +1,13 @@
 function add(parent1, texts){
-    //let parent= document.getElementsByClassName("relative");
      let element = document.createElement("p")
      element.style.marginTop = "1rem";
      element.style.color = "#8c6991"
      let text = document.createTextNode(texts);
      element.append(text);
      parent1.append(element);
-    //let img= parent.getElementsByTagName("img");
     
-    /*
-    for (let i=0; i<parent.length; i++){
-    let element = document.createElement("p");
-    let text = document.createTextNode(texts[i]);
-    element.append(text);
-    parent[i].append(element);
-    //parent.insertBefore(element, img);
-    }*/
     }
-    /*
-    function button(){
-        let parent= document.getElementsByClassName("relative");
-        //let img = parent.querySelector("img");
-        //let attribute = img.setAttribute("src", "assets/images/icon-minus.svg");
-    
-        for(let i=0; i<parent.length; i++){
-            parent[i].querySelector("img").setAttribute("src", "https://images.unsplash.com/photo-1577651559252-9b36597b9ba4?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDk1ODE5MDd8&ixlib=rb-4.0.3&q=85");
-        }
-    }
-    
-    */
+   
     let button1= document.querySelectorAll(".relative img");
     let linked = new Image()
     linked.src="assets/images/icon-minus.svg";
@@ -41,7 +20,7 @@ function add(parent1, texts){
           
             if(paragraph){
               parent1.removeChild(paragraph);
-              this.setAttribute("src", "assets/images/icon-plus.svg" /*linked.src*/ );
+              this.setAttribute("src", "assets/images/icon-plus.svg");
             }
           else{
             let text = 
@@ -69,49 +48,6 @@ function add(parent1, texts){
 
 
 
-
-
-
-/*
-function add(){
-let parent= document.getElementsByClassName("relative");
-//let img= parent.getElementsByTagName("img");
-
-for (let i=0; i<parent.length; i++){
-let element = document.createElement("p");
-let text = document.createTextNode("Hello");
-element.append(text);
-parent[i].append(element);
-//parent.insertBefore(element, img);
-}
-}
-
-function button(){
-    let parent= document.getElementsByClassName("relative");
-    //let img = parent.querySelector("img");
-    //let attribute = img.setAttribute("src", "assets/images/icon-minus.svg");
-
-    for(let i=0; i<parent.length; i++){
-        parent[i].querySelector("img").setAttribute("src", "assets/images/icon-minus.svg");
-    }
-}
-
-
-let button1= document.querySelectorAll(".relative img");
-
-button1.forEach(value =>{
-
-    value.addEventListener("click", function(){
-        add();
-        button();
-    }, false)
-
-})
-
-
-
-
-*/
 
 
 
